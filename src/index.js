db9959cf0137b89d5756e17751b8a05c62f48633
Chris Lee <chrislee910320@gmail.com>
2020-09-24 09:16:43 -0600

    Connect Firebase Environment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'normalize.css';
-import App from './app';
-import {GlobalStyles} from './global-styles';
+import React from "react";
+import { render } from "react-dom";
+import "normalize.css";
+import App from "./app";
+import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
-render(<>
-  <GlobalStyles />
-  <App />
-</>,
+render(
+  <>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
