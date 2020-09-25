3f316ceba425ad976a41dd4c6b144ba11e6b064f
Chris Lee <chrislee910320@gmail.com>
2020-09-25 11:14:47 -0600

    Create loading functions that the page is waiting for profile picture and once the file is ready from Firebase Database, it loads the regular page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,26 +1,47 @@
-import React from 'react'
-import {Link as ReactRouterLink } from 'react-router-dom'
+import React from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
-import {Background, Container, Logo, ButtonLink} from './styles/header'
+import {
+  Background,
+  Container,
+  Logo,
+  ButtonLink,
+  Feature,
+  Text,
+  FeatureCallOut,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
-  return <Container {...restProps}>{children}</Container>
-}
+  return <Container {...restProps}>{children}</Container>;
+};
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
-  return <ButtonLink {...restProps}>{children}</ButtonLink>
-}
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
     <ReactRouterLink to={to}>
       <Logo {...restProps} />
     </ReactRouterLink>
-  )
-}
-
-
+  );
+};
